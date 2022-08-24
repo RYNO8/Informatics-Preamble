c8e870763adff45ac746529ab2f44c4e9400046b
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2022-08-24 11:23:20 +1000

    ranges classes stub

--- src/Ranges.h
+++ src/Ranges.h
@@ -1,11 +1,18 @@
+#include <assert.h>
+#include <limits>
+#include <set>
+#include <vector>
 #pragma once
 
 namespace DS {
-    // @returns A mapping between original coordinates and compressed coordinates
+    // retrieve the underlying value given an iterator
+    // e.g. `value_type<std::vector<int>::begin>` is `int`
     template<typename Iterator>
-    std::map<typename std::iterator_traits<Iterator>::value_type, typename std::iterator_traits<Iterator>::value_type> coordCompressMap(Iterator begin, Iterator end, int startI = 0) {
-        using value_type = typename std::iterator_traits<Iterator>::value_type;
-        std::map<int, int> compressed;
+    using value_type = typename std::iterator_traits<Iterator>::value_type;
+
+    // @returns A mapping between original coordinates and compressed coordinates
+    template<typename T> std::map<value_type<T>, value_type<T>> coordCompressMap(T begin, T end, int startI = 0) {
+        std::map<value_type<T>, value_type<T>> compressed;
 
         int i = startI;
         for (auto val = begin; val != end; ++val, ++i) {
@@ -13,46 +20,212 @@ namespace DS {
         }
         return compressed;
     }
-
-    // @returns A mapping between original coordinates and compressed coordinates
-    template<typename T> std::map<T, T> coordCompressMap(T* begin, T* end, int startI = 0) {
-        std::map<int, int> compressed;
-
-        int i = startI;
-        for (auto val = begin; val != end; ++val, ++i) {
-            compressed[*val] = i;
-        }
-        return compressed;
-    }
-
-    // @returns A mapping between original coordinates and compressed coordinates
-    template<typename T> std::map<T, T> coordCompressMap(std::set<T> toCompress, int startI = 0) {
-        return coordCompressMap(toCompress.begin(), toCompress.end(), startI);
-    }
-
-    // @returns A mapping between original coordinates and compressed coordinates
-    template<typename T> std::map<T, ll> coordCompressMap(std::vector<T> points, int startI = 0) {
-        return coordCompressMap(points.begin(), points.end(), startI);
-    }
-
-
     // Compresses coordinates in place
-    template<typename Iterator> void coordCompressTransform(Iterator begin, Iterator end, int startI = 0){
-        using value_type = typename std::iterator_traits<Iterator>::value_type;
-        std::map<value_type, value_type> m = coordCompressMap(begin, end, startI);
+    template<typename T> void coordCompressTransform(T begin, T end, int startI = 0){
+        std::map<value_type<T>, value_type<T>> m = coordCompressMap(begin, end, startI);
         
-        for (Iterator i = begin; i != end; i++) *i = m[*i];
+        for (T i = begin; i != end; i++) *i = m[*i];
     }
 
-    // Compresses coordinates in place
-    template<typename T> void coordCompressTransform(T* begin, T* end, int startI = 0) {
-        std::map<T, T> m = coordCompressMap(begin, end, startI);
+    // TODO: this is currently a stub
+    // TODO: test
+    template<typename T> class Range {
+        /************************************************
+         *                 INITIALISATION               *
+         ************************************************/
 
-        for (T *i = begin; i != end; i++) *i = m[*i];
-    }
+    private:
+        T lVal, rVal;
+        bool lInclusive, rInclusive;
 
-    // Compresses coordinates in place
-    template<typename T> void coordCompressTransform(std::vector<T> points, int startI = 0) {
-        return coordCompressTransform(points.begin(), points.end(), startI);
-    }
+    public:
+        Range(T lVal, T rVal, bool lInclusive=true, bool rInclusive=true): 
+            lVal(lVal),
+            rVal(rVal),
+            lInclusive(lInclusive),
+            rInclusive(rInclusive) {
+            assert(lVal <= rVal);
+        }
+
+        /************************************************
+         *                    DISPLAY                   *
+         ************************************************/
+
+        std::ostream& print(std::ostream &out = std::cout, bool newLine=false) const {
+            out << (lInclusive ? '[' : '(');
+            out << lVal << ", " << rVal;
+            out << (rInclusive ? ']' : ')');
+            if (newLine) out << '\n';
+            return out;
+        }
+
+        /************************************************
+         *                  PROPERTIES                  *
+         ************************************************/
+
+        const T& l() const {
+            return &lVal;
+        }
+
+        const T& r() const {
+            return &rVal;
+        }
+
+        T length() const {
+            return (
+                rVal - 
+                lVal - 
+                (rInclusive ? std::numeric_limits<T>::lowest() : 0) - 
+                (lInclusive ? std::numeric_limits<T>::lowest() : 0)
+            );
+        }
+
+        bool isNull() const {
+            return length() == 0;
+        }
+
+        /************************************************
+         *                  OPERATIONS                  *
+         ************************************************/
+
+        // should call this covers?
+        bool contains(const Range<T> &o) const {
+            return true;
+        }
+
+        bool touches(const Range<T> &o) const  {
+            return true;
+        }
+
+        bool overlaps(const Range<T> &o) const  {
+            return true;
+        }
+
+        Range<T> intersect(const Range<T> &o) const {
+            return true;
+        }
+
+        Range<T> operator*(const Range<T> &o) const {
+            return intersect(o);
+        }
+
+        bool operator<(const Range<T> o) const {
+            if (lVal == o.lVal) return rVal < o.rVal;
+            return lVal < o.lVal;
+        }
+
+        bool operator>(const Range<T> o) const {
+            if (lVal == o.lVal) return rVal > o.rVal;
+            return lVal > o.lVal;
+        }
+
+        bool operator<=(const Range<T> o) const {
+            if (lVal == o.lVal) return rVal <= o.rVal;
+            return lVal <= o.lVal;
+        }
+
+        bool operator>=(const Range<T> o) const {
+            if (lVal == o.lVal) return rVal >= o.rVal;
+            return lVal >= o.lVal;
+        }
+
+        /************************************************
+         *               TRANSFORMATIONS                *
+         ************************************************/
+        std::pair<Range<T>, Range<T>> split(T val) {
+
+        }
+    };
+    
+    /************************************************
+	 *                    DISPLAY                   *
+	 ************************************************/
+	template<typename T> std::ostream& operator<<(std::ostream& out, const Range<T>& val) {
+		val.print(out);
+		return out;
+	}
+
+    // TODO: this is currently a stub
+    // TODO: test
+    template<typename T> class Ranges: public std::set<Range<T>> {
+        /************************************************
+         *                 INITIALISATION               *
+         ************************************************/
+
+    private:
+
+    public:
+        // inherit constructors and all constructor overloads
+        using std::set<Range<T>>::set;
+
+        // NOTE: dont need print because c++ will call print on the underlying set
+
+        void _cleanup() {
+            // merge ranges when
+        }
+
+        /************************************************
+         *                  OPERATIONS                  *
+         ************************************************/
+
+        // should call this covers?
+        bool contains(const Ranges<T> &o) const {
+            return true;
+        }
+
+        bool overlaps(const Ranges<T> &o) const  {
+            return true;
+        }
+
+        Ranges<T> getIntersect(const Ranges<T> &o) const {
+            return true;
+        }
+
+        Ranges<T> operator*(const Ranges<T> &o) const {
+            return getIntersect(o);
+        }
+
+        Ranges<T> getUnion(const Ranges<T> &o) const {
+            return true;
+        }
+
+        Ranges<T> operator+(const Ranges<T> &o) const {
+            return getUnion(o);
+        }
+
+        Ranges<T> getComplement(T rangeStart = std::numeric_limits<T>::min(), T rangeEnd = std::numeric_limits<T>::max()) const {
+
+        }
+        Ranges<T> operator~() const {
+            return getComplement();
+        }
+
+        bool isDisjoint() const {
+            return true;
+        }
+
+        Ranges<T> flatten() const {
+
+        }
+
+        /************************************************
+         *               TRANSFORMATIONS                *
+         ************************************************/
+
+        Range<T> split(T val) {
+
+        }
+
+        Range<T> splits(std::vector<T> val) {
+
+        }
+    };
+
+    /************************************************
+	 *                    DISPLAY                   *
+	 ************************************************/
+	// template<typename T> std::ostream& operator<<(std::ostream& out, const Ranges<T>& val) {
+	// 	val.print(out);
+	// 	return out;
+	// }
 };
--- tests/ranges.test.cpp
+++ tests/ranges.test.cpp
@@ -1,11 +1,23 @@
 ﻿#include "../src/Constants.h"
 #include "../src/Ranges.h"
 using namespace std;
+using namespace DS;
 
 void testRanges() {
-	vector<int> a = { 0,6,4, 4, 0, -1 };
-	coordCompressTransform(a);
-	cout << a;
+	/*vector<int> a = { 0,6,4, 4, 0, -1 };
+	coordCompressTransform(a.begin(), a.end());
+	cout << a << "\n";
+
+	int b[] = { 0,6,4, 4, 0, -1 };
+	coordCompressTransform(begin(b), end(b));
+	printArr0(b, 6);
+	cout << "\n";*/
+
+	/*Range<int> r = {1, 2};
+	cout << r << " " << (r < r);*/
+
+	Ranges<int> r = { {1, 2}, {3, 4} };
+	cout << r.size();
 }
 
 

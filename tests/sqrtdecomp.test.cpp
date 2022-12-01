857796ea1a2d32f1f5f8a35902f81a993df38c34
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2022-12-01 23:36:21 +1100

    minor fixes to mo's

--- tests/sqrtdecomp.test.cpp
+++ tests/sqrtdecomp.test.cpp
@@ -1,5 +1,9 @@
-﻿#include "../src/Constants.h"
+﻿// solution to Omo
+// https://orac.amt.edu.au/cgi-bin/train/problem.pl?problemid=1102
+#include "../src/Constants.h"
 #include "../src/SqrtDecomp.h"
+#include "../src/Ranges.h"
+using namespace DS;
 using namespace std;
 
 void testSqrtDecomp() {
@@ -7,13 +11,13 @@ void testSqrtDecomp() {
 	vector<pair<int, int>> queries;
 
 	cin >> N >> Q;
-	for (int i = 1; i <= N; ++i) cin >> arr[i];
+	for (int i = 0; i < N; ++i) cin >> arr[i];
 	for (int a, b, i = 0; i < Q; ++i) {
 		cin >> a >> b;
-		queries.push_back({ a, b });
+		queries.push_back({ a - 1, b - 1 });
 	}
 
-	coordCompressTransform(arr + 1, arr + N + 1);
+	coordCompressTransform(arr, arr + N);
 
 	function<void(int)> add = [&](int i) {
 		if (curr[arr[i]] % 2 == 0) ++bad;
@@ -31,7 +35,7 @@ void testSqrtDecomp() {
 		return bad == ((r - l + 1) % 2 == 1);
 	};
 
-	for (bool ans : chunkQueries(queries, add, rem, answer)) cout << (ans ? "YES" : "NO") << '\n';
+	for (bool ans : chunkQueries<int, bool>(queries.begin(), queries.end(), add, rem, answer)) cout << (ans ? "YES" : "NO") << '\n';
 }
 
 signed main() {
